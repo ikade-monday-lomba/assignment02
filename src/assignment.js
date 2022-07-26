1e3fabbb9e30884f8891728ae0edac9458a0e1de
ikade-monday-lomba <109315468+ikade-monday-lomba@users.noreply.github.com>
2022-07-26 14:07:06 +0000

    test-challenge1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,11 +13,34 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+const numbers = [
+    68,
+    -68,
+    27,
+    94,
+    72,
+    -25,
+    -51,
+    32,
+    10,
+    64,
+    -94,
+    4,
+    34,
+    -86,
+    90,
+    81,
+    20,
+    -56,
+    -91,
+    -50
+];
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    var startCount=0;
+    let sumOfArray=arrayOfNumbers.reduce((accum,currentValue)=>accum+currentValue,startCount);
+    return sumOfArray;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
-
+assignment.sumOfNumbers=sumOfNumbers
 /**
  * Challenge - 2
  * 
@@ -27,11 +50,40 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
 
+    const digits = [
+        17,
+        0,
+        67,
+        41,
+        49,
+        21,
+        1,
+        86,
+        76,
+        2,
+        54,
+        14,
+        93,
+        84,
+        14,
+        77,
+        35,
+        7,
+        29,
+        64
+    ]
+        function countEvenNumbers(arrayOfNumbers) {
+        var arr =[]
+        for(let index=0; index<=arrayOfNumbers.length; index++){
+            if(arrayOfNumbers[index]%2===0){
+                arr.push(arrayOfNumbers[index]);
+            }
+        }
+        console.log(arr)
+        return arr.length;
+        }
+assignment.countEvenNumbers=countEvenNumbers
 /**
  * Challenge - 3
  * 
@@ -41,7 +93,7 @@ function countEvenNumbers(arrayOfNumbers) {
  * E.g 51.21 should just be 51 (hint: Math.trunc(...) function)
  * If you did Challenge - 3, remove the comment in the line just after this function
  * 
- * See https://www.thoughtco.com/celcius-to-farenheit-formula-609227 for the conversion formula
+ * See https://www.thoughtco.com/celcius-to-farenheit-formula-609(227 for the conversion formula
  * 
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
